424d69541219c2fdeb03b3cd35e661f3cab81b85
Christina Takara <christina.takara@gmail.com>
2023-11-09 13:06:51 -0600

    added class example files

---
+++ src/components/UncontrolledForm.jsx
@@ -0,0 +1,67 @@
+import { useRef } from "react";
+
+function UncontrolledForm () {
+// ref to get input values
+const nameInput = useRef(null);
+const ageInput = useRef(null);
+
+const handleSubmit = event => {
+  // prevent page refresh
+  event.preventDefault()
+  // do what you want with the form data
+  console.log({
+    name: nameInput.current.value,
+    age: ageInput.current.value,
+  })
+}
+
+    return (
+        <div>
+        <h2>Uncontrolled Form goes here</h2>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                ref={nameInput} 
+                placeholder="write name here" 
+            />
+            <input 
+                type="number"
+                ref={ageInput}
+                placeholder="write age here"
+            />
+            <input type="submit" value="Submit Form" />
+        </form>
+        </div>
+
+
+    )
+}
+
+export default UncontrolledForm;
+
+// import { useRef } from "react"
+
+// const Form = props => {
+//   // ref to get input values
+//   const nameInput = useRef(null)
+// const ageInput = useRef(null)
+
+// const handleSubmit = event => {
+//   // prevent page refresh
+//   event.preventDefault()
+//   // do what you want with the form data
+//   console.log({
+//     name: nameInput.current.value,
+//     age: ageInput.current.value,
+//   })
+// }
+
+// // The JSX for the form binding the functions and state to our inputs
+// return (
+//   <form onSubmit={handleSubmit}>
+//     <input type="text" ref={nameInput} placeholder="write name here" />
+//     <input type="number" ref={ageInput} placeholder="write age here" />
+//     <input type="submit" value="Submit Form" />
+//   </form>
+// )
+// }
